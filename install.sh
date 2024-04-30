e200901252b2f642ab3f54fa59e0305a178f7130
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-30 12:51:01 +0200

    Remove install log

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ sleep 1
 touch install.log
 
 echo "> Installing packages..."
-apt update -y >> install.log
-apt install curl wget sudo htop wget -y >> install.log
+apt update -y
+apt install curl wget sudo htop wget -y
 
 echo "> Adding repo..."
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
@@ -27,18 +27,18 @@ echo "> Adding repo key..."
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
 
 echo "> Installing packages..."
-apt update -y >> install.log
-apt full-upgrade -y >> install.log
+apt update -y
+apt full-upgrade -y
 
 echo "> Installing proxmox kernel..."
-apt install proxmox-default-kernel -y >> install.log
-apt install proxmox-ve postfix open-iscsi chrony -y >> install.log
+apt install proxmox-default-kernel -y
+apt install proxmox-ve postfix open-iscsi chrony -y
 
 echo "> Remove debian kernel"
-apt remove linux-image-amd64 'linux-image-6.1*' -y >> install.log
+apt remove linux-image-amd64 'linux-image-6.1*' -y
 
 echo "> Remove os probe"
-apt remove os-prober -y >> install.log
+apt remove os-prober -y
 
 echo "> Install done!"
 echo "> Rebooting server in 5 seconds"
